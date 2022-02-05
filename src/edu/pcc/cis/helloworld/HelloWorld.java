2c4bc50519f2bf6d6c6951b4fe419617a025e9fa
Lukecommits <lukeslinkert@gmail.com>
2022-02-04 22:34:39 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,11 +1,13 @@
+package edu.pcc.cis.helloworld;
+import java.util.Date;
 /**
  * @author Luke Slinkert
  * @version 2022.02.04
  */
-package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
+        System.out.println(new Date());
     }
 }
